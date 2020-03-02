385c73b9a7023cd903aee907d3756950d3222f8c
dylan0000 <58610746+dylan0000@users.noreply.github.com>
2020-03-02 13:13:10 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+//Part II
+
+// On Arcade, write a list of your five favorite DESSERTS
+
+// EX:  flan, brownie, pie, lava cake, tiramisu
+
+// Store DESSERTS an an Array List
+
+let desserts = ["flan", "brownie", "pie", "lava cake", "tiramisu"]
+
+// Find & display the first item: 
+
+game.splash(desserts[0])
+
+// Find & display the last item: ________________________________
+game.splash(desserts[desserts.length - 1])
+
+// Find & display the third item: ________________________________
+game.splash(desserts[desserts.length - 3])
+
+// Replace & display the second item with "popcorn":
+
+desserts[1] = "popcorn"
+game.splash(desserts[1])
+
+// Replace the last item with a friend's favorite dessert
+desserts[4] = "ice cream"
+game.splash(desserts[4])
